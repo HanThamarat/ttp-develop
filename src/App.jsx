0a0a74ad0fad3b335279cf6c19d71dccbab74d73
HanThamarat <HanThamarat@gmail.com>
2024-05-28 08:54:09 +0700

    init project ttpdevelop

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/headers/nav"
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/headers/nav.jsx
@@ -0,0 +1,22 @@
+import Headroom  from 'react-headroom';
+
+const Navbar = () => {
+  return (
+    <Headroom>
+        <nav className='bg-white px-10 py-8'>
+            <div className='text'>
+                <span>Longtail Dev</span>
+            </div>
+            <div>
+
+            </div>
+        </nav>
+    </Headroom>
+  );
+};
+
+Navbar.propTypes = {
+
+};
+
+export default Navbar;
